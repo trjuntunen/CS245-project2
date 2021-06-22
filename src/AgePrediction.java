b1b2645e2fefe556ac1e7394800c34278dd33386
Teddy Juntunen <trjuntunen@gmail.com>
2021-06-21 18:07:30 -0700

    initial commit

---
+++ src/AgePrediction.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class AgePrediction {
+
+    // ArrayList<Person> people;
+
+    void readConfig() {
+
+    }
+
+    void fillListWithData() {
+        // read in config file
+        // loop through each file in given directory in config file
+        // read in data from file
+//        try (BufferedReader reader = new BufferedReader(new FileReader())) {
+//
+//        }
+
+    }
+
+}
---
+++ src/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+
+    private String name;
+    private int age;
+    private String gender;
+    private String state;
+    private int year;
+    private int nameCount;
+
+    public Person(String name, int age, String gender, String state, int year, int nameCount) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.state = state;
+        this.year = year;
+        this.nameCount = nameCount;
+    }
+
+}
